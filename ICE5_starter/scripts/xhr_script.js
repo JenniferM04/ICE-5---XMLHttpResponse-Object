c53cba5ad7f35713fe6bc7e29b2a5e863d79aaa5
Jennifer M <156385936+JenniferM04@users.noreply.github.com>
2024-02-16 17:06:22 -0500

    Updating html elements
    Added Ids to the figcaptions

--- ICE5_starter/scripts/xhr_script.js
+++ ICE5_starter/scripts/xhr_script.js
@@ -16,15 +16,32 @@ let url_users = 'https://jsonplaceholder.typicode.com/users';
 let xhr = new XMLHttpRequest();
 
 // create a callback function to fire when the onreadystatechange happens
+xhr.onreadystatechange = function () {
     // check that the state is done
-    // if (xhr.readyState === 4) {
+    if (xhr.readyState === 4) {
         // turn into json
-        // update the 1st image
-        // update the 1st figcaption
-        // update the 2nd image
-        // update the 2nd figcaption
-        // send error message
+        let responseData = JSON.parse(xhr.responseText);
 
+        // update the 1st image
+        let photo1 = document.getElementById('photo1');
+        photo1.src = responseData[0].url;
+
+        // update the 1st figcaption
+        let figcaption1 = document.getElementById('figcaption1');
+        figcaption1.textContent = responseData[0].title;
+
+        // update the 2nd image
+        let photo2 = document.getElementById('photo2');
+        photo2.src = responseData[1].url;
+
+        // update the 2nd figcaption
+        let figcaption2 = document.getElementById('figcaption2');
+        figcaption2.textContent = responseData[1].title;
+
+        // send error message
+        // Add your logic to handle errors here if needed
+    }
+};
 // use the .open() method to configure the object
 
 // add datatype to header
