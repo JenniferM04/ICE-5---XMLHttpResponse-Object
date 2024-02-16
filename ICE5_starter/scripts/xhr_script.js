5f5a2da8791484f79c7c09e64328aaf9c6c812dd
Jennifer M <156385936+JenniferM04@users.noreply.github.com>
2024-02-16 17:04:28 -0500

    Html Object
    creating a new XMLHttpRequest object.

--- ICE5_starter/scripts/xhr_script.js
+++ ICE5_starter/scripts/xhr_script.js
@@ -1,26 +1,29 @@
-console.log('xhr_script.js loaded')
+/**
+ * Jennifer Murphy
+ * February 16, 2024
+ */
+
+console.log('xhr_script.js loaded');
 
 let url_posts = 'https://jsonplaceholder.typicode.com/posts';
 let url_comments = 'https://jsonplaceholder.typicode.com/comments';
 let url_albums = 'https://jsonplaceholder.typicode.com/albums';
-let url_photos = 'https://jsonplaceholder.typicode.com/photos';
+let url_photos = 'https://jsonplaceholder.typicode.com/photos'; // USE THIS FOR 1ST AND 2ND IMAGE
 let url_todos = 'https://jsonplaceholder.typicode.com/todos';
 let url_users = 'https://jsonplaceholder.typicode.com/users';
 
 // instantiate the XMLHttpRequest object with the new keyword
-
-
+let xhr = new XMLHttpRequest();
 
 // create a callback function to fire when the onreadystatechange happens
     // check that the state is done
     // if (xhr.readyState === 4) {
-            // turn into json
-            // console log to see what we have
-            // update the 1st image
-            // update the 1st figcaption
-            // update the 2nd image
-            // update the 2nd figcaption
-            // send error message
+        // turn into json
+        // update the 1st image
+        // update the 1st figcaption
+        // update the 2nd image
+        // update the 2nd figcaption
+        // send error message
 
 // use the .open() method to configure the object
 
